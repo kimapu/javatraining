4f1fb98bcb94d5a9e84a5c7d56d52101ccf3b679
kimapu <kimlee@staffemail.apu.edu.my>
2021-09-27 14:00:49 +0800

    app.java update

---
+++ myjavatraining/src/my/apiit/training/main/App.java
@@ -0,0 +1,14 @@
+package my.apiit.training.main;
+
+public class App {
+	
+	/**
+	 * COMMENT: Main method
+	 * Method signature: public static void main( String[] )
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println( "Hello World!" );
+	}
+	
+}
