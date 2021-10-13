367eec451def40f2dc182a04c3760d8233860eab
kimapu <kimlee@staffemail.apu.edu.my>
2021-10-13 12:51:59 +0800

    score computer

---
+++ myjavatraining/src/my/apiit/training/impl/Score.java
@@ -0,0 +1,56 @@
+package my.apiit.training.impl;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Score {
+
+	public final String DB_FILE = "src/my/apiit/training/impl/scores.txt"; 
+	
+	private String targetFile;
+	public Score( String f ) {
+		targetFile = f;
+	}
+
+	public long sum() throws NumberFormatException{
+		long sum = 0;
+		List<String> scores = getAll( targetFile );
+		if ( scores != null && scores.size() > 0 ) {
+			for (String scoreLine : scores) {
+				StringTokenizer tk = new StringTokenizer( scoreLine );
+				while ( tk.hasMoreTokens() ) {
+					sum += Double.parseDouble( tk.nextToken() );
+				}
+			}
+		} else {
+			System.out.println( "No scores!" );
+		}
+		return sum;
+	}
+	
+	private List<String> getAll( String fileName ) {
+		List<String> content = null;
+		if( new File(fileName).exists() ) {
+			content = new ArrayList<>();
+			try {
+				BufferedReader br = new BufferedReader( new FileReader(fileName) );
+				String line;
+				while( (line = br.readLine()) != null ) {
+					content.add( line );
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {
+				System.out.println( "Done..." );
+			}
+		}
+		return content;
+		
+	}
+	
+}
